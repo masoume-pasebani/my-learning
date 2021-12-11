792f0377e6edeff43e609801969b7e917b270463
Masi <81412933+masoume-pasebani@users.noreply.github.com>
2021-12-11 20:34:01 +0330

    Add files via upload

---
+++ DL/tb.v
@@ -0,0 +1,47 @@
+`include "pla.v"
+
+module pla_tb();
+    reg x,y,z;
+    wire A,B,C,D;
+
+   pla pla(A,B,C,D,x,y,z);
+   initial begin
+        x=0;
+        y=0;
+        z=0;
+        #30;
+        x=0;
+        y=0;
+        z=1;
+        #30;
+        x=0;
+        y=1;
+        z=0;
+        #30;
+        x=1;
+        y=0;
+        z=0;
+        #30;
+        x=0;
+        y=1;
+        z=1;
+        #30;
+        x=1;
+        y=0;
+        z=1;
+        #30;
+        x=1;
+        y=1;
+        z=0;
+        #30;
+        x=1;
+        y=1;
+        z=1;
+        #30;
+        x=0;
+        y=0;
+        z=0;
+        #30;
+  end
+
+endmodule
